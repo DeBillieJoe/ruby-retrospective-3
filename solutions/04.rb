dd984c26f560c468c9e75df2f4dd6aed48266eb7
Ilia Tobov <ilia_tobov@mai.bg>
2014-01-21 17:18:24 +0200

    FIx typos and add class Storage for operations and labels

--- solutions/04.rb
+++ solutions/04.rb
@@ -1,29 +1,29 @@
 module Asm
   module Instructions
     def mov(destination_register, source)
-      @registers[destination_register] = get_value source
+      @registers[destination_register] = get_actual_value(source)
     end
 
     def inc(destination_register, value = 1)
-      @registers[destination_register] += get_value value
+      @registers[destination_register] += get_actual_value(value)
     end
 
     def dec(destination_register, value = 1)
-      @registers[destination_register] -= get_value value
+      @registers[destination_register] -= get_actual_value(value)
     end
 
     def cmp(register, value)
-      @last_comparison = @registers[register] <=> get_value value
+      @last_comparison = (@registers[register] <=> get_actual_value(value))
     end
 
-    def get_value(value)
+    def get_actual_value(value)
       @registers[value] or value
     end
   end
 
   module Jumps
     def jmp(where)
-      @pointer = @labels[where] or where
+      @pointer = (@labels[where] or where)
     end
 
     jumps = {
@@ -37,53 +37,58 @@ module Asm
 
     jumps.each do |jump_name, comparison|
       define_method jump_name do |where|
-        @last_comparison.public_send(comparison, 0) ? (return jmp where) : @pointer = @pointer.succ
+        @last_comparison.public_send(comparison, 0) ? (return jmp(where)) : @pointer = @pointer.succ
       end
     end
+  end
 
   class Evaluator
-
     include Instructions
     include Jumps
 
-    def initialize
-      @registers = {ax: 0, bx: 0, cx: 0, dx: 0}
-      @last_comparison = 0
-      @operations = []
-      @labels = {}
-      @pointer = 0
-    end
-  end
+    class Storage
+      attr_reader :labels, :operations
 
-  def self.perform_operations(evaluator)
-    evaluator.operations_queue.each do |operation|
-      if operation[0] == :'='
-        evaluator.send operation[1].name+"=", get_value(operation[2])
-      else
-        is_cmp?(operation, evaluator)
+      def initialize(&block)
+        @operations = []
+        @labels = {}
+        instance_eval(&block)
+      end
+
+      def method_missing(method_name, *args)
+        if (Instructions.instance_methods + Jumps.instance_methods).include? method_name
+          @operations << [method_name, args]
+        else
+          method_name.to_sym
+        end
+      end
+
+      def label(label_name)
+        @labels[label_name] = @operations.size
       end
     end
-  end
 
-  private
-
-  def self.is_cmp?(operation, evaluator)
-    if operation[0] == :'<=>'
-      evaluator.last_cmp = operation[1].value.send operation[0], get_value(operation[2])
-    else
-      operation[1].value = operation[1].value.send operation[0], get_value(operation[2])
+    def initialize(&block)
+      @registers = {ax: 0, bx: 0, cx: 0, dx: 0}
+      @last_comparison = 0
+      @pointer = 0
+      storage = Storage.new(&block)
+      @operations = storage.operations
+      @labels = storage.labels
     end
-  end
 
-  def self.get_value(value)
-      value.nil? ? 1 : ((value.is_a? Integer) ? value : value.value)
+    def evaluate
+      until @pointer == @operations.size
+        method_name = @operations[@pointer].first
+        args        = @operations[@pointer].last
+        public_send(method_name, *args)
+        @pointer = @pointer.succ if !Jumps.instance_methods.include? method_name
+      end
+      @registers.values
+    end
   end
 
   def self.asm(&block)
-    evaluator = Evaluator.new
-    evaluator.instance_eval(&block)
-    perform_operations(evaluator)
-
-    evaluator.registers.map { |register| register.value }
+    Evaluator.new(&block).evaluate
   end
 end
